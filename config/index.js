6e7ad613116b5dd4477bf7a41f11e0622657008a
daniel cawen <cawendaniel@gmail.com>
2026-04-24 14:58:59 -0300

    add db connection

---
+++ config/index.js
@@ -0,0 +1,7 @@
+export const dbConfig = {
+  host:     process.env.DB_HOST,
+  port:     parseInt(process.env.DB_PORT, 10),
+  database: process.env.DB_NAME,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+};
